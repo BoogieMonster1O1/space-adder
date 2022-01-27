c88f3dd97e52bd2d860c5eb06541507a77cf3110
BoogieMonster1O1 <shrish.deshpande@icloud.com>
2022-01-27 12:06:31 +0530

    stupid

--- lol.js
+++ lol.js
@@ -2,7 +2,7 @@ function bruh() {
     var input = document.getElementById("in").value;
     var out = "";
     for (let i in input) {
-        out += str[i];
+        out += input[i];
         if (i < input.length - 1) {
             out += " ";
         }
